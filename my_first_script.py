cd718ccd45b48dd9955eaae814a5b7d2962b58c2
Windsorda <windsorda@hotmail.com>
2025-11-28 15:08:16 -0500

    Add files via upload

---
+++ my_first_script.py
@@ -0,0 +1,26 @@
+#print ("Hello World!!")
+#print ("I can write in python code")
+       
+# Day 2 Assignment - Exercise 1
+
+#milk = 3.50
+#bread = 2.49
+#cleareggs = 4.50
+# total_cost = (milk + bread + eggs)
+#X = (total_cost)
+#print ("Your total is: $", X)
+
+# Day 2 Assignment - Exercise 2
+
+#store_name = input("Whats your favorite grocery store? ") # Ask user for input
+#print ("Welcome to your favorite grocery store:", store_name) # Greet the user with their input
+
+# Day 2 Assignment - Exercise 3
+
+# The program has an error, find it and fix it
+#milk = "3"
+milk = 3
+bread = 2.5
+total = milk + bread
+#print("The total cost is: $" + total)
+print("The total cost is: $", total)
